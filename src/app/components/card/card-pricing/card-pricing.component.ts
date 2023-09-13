cf2fa5ea833bdc2284900131cca0f39470d1c2fc
Arthur Ercan <arthurtaylan@hotmail.com>
2023-09-12 21:03:24 -0300

    card pricing component modified

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class CardPricingComponent {
 
+  @Input()
+  gameType:string = "Digital PS4"
+  @Input()
+  gameprice:string = "R$ 399.90"
 }
